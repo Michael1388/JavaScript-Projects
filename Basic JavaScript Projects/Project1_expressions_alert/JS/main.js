73f89906187074f87fe05a0452da60959fcc0793
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2022-12-19 15:48:11 -0600

    Update JS Folder

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+
+
+window.alert("Hello Tech Academy Instructor"); // creat a window.alert 
+
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string." // create two variables
+document.write(Sent1+Sent2); //Concatenate 2 variables and "document.write" the expression of them both."
+
