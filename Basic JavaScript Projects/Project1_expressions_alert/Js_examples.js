587f7b3f47497ba090707a5f49a433fa621c8c0a
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2022-12-19 15:41:47 -0600

    JavaScript Folder update

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Js_examples.js
@@ -0,0 +1,39 @@
+alert("Hello World");
+
+window.alert("Hello World");
+
+var A = "This is a string";
+
+document.write(A);
+
+var A = "This \"is\" a string"; 
+
+document.write("\"Be who you are and say what you feel," 
++ " because those who mind don\'t matter and those who matter don\'t mind. \""
++ "-Dr.Seuss");
+
+var Family = "The Arezzinis", Dad =  "Jeremiah", Mom = "Hermoine",
+Daughter = "Penny", Son = "Zorro";
+
+document.write(Dad);
+
+var Family = "The La Roccas", Dad =  "Michael", Mom = "Holly",
+Son = "Ethan", Son_2 = "Joshua";
+
+document.write(Son_2);
+
+document.write(1+3);
+
+document.getElementById
+
+function My_Function () {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+// Comment code in JS
+
+function My_Function () { // creating a function
+    var str = "This is the button text!"; // creating a returnable string
+    document.getElementById("Button_Text").innerHTML = str; //identifying the response 
+}
