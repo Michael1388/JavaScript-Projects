776720dcec949dd83c3f5f0a628e0b6f469d5e2b
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2022-12-19 17:07:35 -0600

    Update JavaScript folder

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Events/sample_events.js
@@ -0,0 +1,6 @@
+
+
+function My_Function () { // creating a function
+    var str = "Too slow try again!"; // creating a returnable string
+    document.getElementById("Button_Text").innerHTML = str; //identifying the response 
+}
---
+++ Basic JavaScript Projects/Projects2_functions/JS/main.js
@@ -0,0 +1,7 @@
+
+
+function Button_Function () {
+    var str = "Too slow, try again!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
