0f23430295daf46a407a4a06d368f6ac7f1ebfc5
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2022-12-20 13:41:04 -0600

    Upload Project 3 math operators folder contents

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,20 @@
+
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = " " + addition;
+}
+
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("subtract").innerHTML =" "+ Subtraction;
+}
+
+function multiply() {
+    var m_m = 6 * 8;
+    document.getElementById("times").innerHTML = " " + m_m;
+}
+
+function division() {
+    var d_d = 48 / 6;
+    document.getElementById("divide").innerHTML = " " + d_d;
+}
