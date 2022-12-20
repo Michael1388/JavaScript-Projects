f1e1222509912d4e87f593dea26775b43f61916e
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2022-12-20 10:28:24 -0600

    Update Project 2 functions folder

--- Basic JavaScript Projects/Projects2_functions/JS/main.js
+++ Basic JavaScript Projects/Projects2_functions/JS/main.js
@@ -1,7 +1,13 @@
 
 
-function Button_Function () {
-    var str = "Too slow, try again!";
-    document.getElementById("Button_Text").innerHTML = str;
+function Button_Function () { /*create a new function and name it */
+    var str = "Too slow, "; /* define a variable */
+    var confuse = " Refresh and Try again!"; /* define a second variable */
+    document.getElementById("Button_Text").innerHTML = str + confuse; /* define the id and method of the element we want called, concatrenate the two variables */
 }
 
+function myFunction() { /*create a new function and name it */
+    var sentence = "I am learning a lot from this book!"; /* creat a variable to call into the HTML file*/
+    sentence += " There is no winning the above button challenge, sorry. I am still learning!"; /* concatenante a string of text to the previous string of text */
+    document.getElementById("Concatenate").innerHTML = sentence; /* define the id and method of the element we want called from the HTML file*/
+}
