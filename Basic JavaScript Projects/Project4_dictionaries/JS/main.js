9b0928c2b4106b3bf60159b8064dc0edefd82080
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2022-12-21 13:56:04 -0600

    Update JavaScript file "main"

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -7,8 +7,11 @@ function animal_Dictionary() { /*define a function called "animal_Dictionary" to
         Color:"Black",          /* Assign KVP */
         Breed:"Labrador",       /* Assign KVP */
         Age:5,                  /* Assign KVP , we do not have to place 5 in " " because 5 is a value "type" number. */
-        Sound:"Bark!"           /* Assign KVP */
+        Sound:"Bark!",           /* Assign KVP */
+        Sound:"Rouf!"    /* Assign IDENTICAL KEY to OBSERVE the result */
     };
+    delete Animal.Sound; /* deletes the Key "Sound" and because it now no longer exists will return "undefined" whne called by our HTML code request for this result*/
     document.getElementById("BlackDog").innerHTML = Animal.Sound; /*  create a method by which to call our result from the HTML file
      with the Element id of "BlackDog" to call our "animal_Dictionary" Function result request of the value of our Object, "Animal" Key "Sound" whose value which will be returned is "Bark!" */
 }
+
